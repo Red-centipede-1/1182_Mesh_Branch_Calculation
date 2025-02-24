b2fb5770b27e0586ea7074a28b727809ad62d90f
Red-Centipede-1 <nw5544n@gre.ac.uk>
2025-02-24 12:06:12 +0000

    completed, need to check calculation to make sure I values are correct, main error before was divide by 0 for the characteristic determinate, this is because =+ operator was used which doesnt exist in matlab, also -1^(I+1) didnt work in determinate function because in matlab this interpretes as (-)(1^(I+1) so changed to (-1)^(I+1).

--- Lab4.m
+++ Lab4.m
@@ -1,5 +1,5 @@
 % --- Task 1 --- 
-% % %prompt user to enter input values
+% % % %prompt user to enter input values
 % 
 % A = input('value for A = ');
 % B = input('value for B = ');
@@ -19,11 +19,8 @@
 
 %construct a function to find a minor 
 
-function [minor] = find_minor(matrix, element) 
+function [minor] = find_minor(matrix, row, column) 
     minor_matrix = matrix; % set varaible for minor matrix as copy of original matrix        
-   
-    % search matrix for element, assign row and column variables to row and column of element 
-    [row, column] = find(matrix == element);  
     minor_matrix(row,:) = []; % remove row of element from matrix (replaces each element in row with empty element)
     minor_matrix(:,column) = []; % remove column of element from matrix (replaces each element in column with empty element)
     % calculate minor of new array using equation for minor
@@ -42,22 +39,21 @@ function [det] = find_determinate(matrix)  % find determinate using elements in 
         % cycles from 2 to 4
         % det =+ matrix(1,I) * find_minor(matrix, matrix(1,I)) * cofactor
 
-        Cofactor = -1^(I+1); % Cofactor variable
-        Element = matrix(1,I); % Element variable
-        minor = find_minor(matrix, Element); % use find minor function to find minor
-        det =+ Element * Cofactor * minor; % multiply and increment determiante
-
+        cofactor = (-1)^(I+1); % Cofactor variable,
+        element = matrix(1,I); % Element variable
+        minor = find_minor(matrix, 1, I); % use find minor function to find minor of top row elements
+        det = det + (element * cofactor * minor); % multiply and increment determiante
     end
 end
 
-determinate0 = find_determinate(matrix0);
+%determinate0 = find_determinate(matrix0);
 
 % --- Task 2 ---
 % calculation of branch current by mesh analysis and Cramer's Rule
 
 V1 = 6; % Set voltage value as 6V
 
- % Set Value for Resistors in 𝛺
+% Set Value for Resistors in 𝛺
 R1 = 330; % Set Value for R1 
 R2 = 330; % Set Value for R2
 R3 = 330; % Set Value for R3
@@ -76,9 +72,8 @@ D1 = -R2;
 E1 = R2 + R3 + R4;
 F1 = -R4;
 G1 = 0;
-H1 = R4;
+H1 = -R4;
 I1 = R4 + R5 + R6;
-
 
 % Construct matrix seperate columns with space and rows with semicolon
 matrix1 = [A1 D1 G1; B1 E1 H1; C1 F1 I1];
@@ -94,16 +89,19 @@ function[char_det] = find_characteristic_determinate(matrix)
         % cycles from 2 to 4
         % det =+ matrix(1,I) * find_minor(matrix, matrix(1,I)) * cofactor
 
-        Cofactor = 1^(I+1); % Cofactor variable
+        Cofactor = (1)^(I+1); % Cofactor variable
         Element = matrix(I,1); % Element variable
-        minor = find_minor(matrix, Element); % use find minor function to find minor
-        char_det =+ (Element * Cofactor * minor); % multiply and increment determiante
+        minor = find_minor(matrix, I, 1); % use find minor function to find minor of top row elements
+        
+        char_det = char_det + (Element * Cofactor * minor); % multiply and increment determiante
     end
 end
 % Use function from before to employ Cramers rule
 
+det1 = find_determinate(matrix1);
+disp(det1);
 
-function [I1, I2, I3] = Branch_Current_Calculation(voltage, matrix)
+function [Current] = Branch_Current_Calculation(voltage, matrix)
     % Set starting I values so a list can be initialized with them
     I1 = 0;
     I2 = 0;
@@ -112,22 +110,29 @@ function [I1, I2, I3] = Branch_Current_Calculation(voltage, matrix)
 
     %char_det = find_characteristic_determiante(matrix);
     char_det = find_determinate(matrix);
+    disp(char_det);
 
-    for I = 1:3 % for each top row element of array 
+    for I = 1:3 % loop 3 times for each i value and element in row
 
         % use find_minor function to find the minor of top three elements
         % of matrix, multiply that number by cofactor = -1^(I+1) where I
         % cycles from 2 to 4
         % det =+ matrix(1,I) * find_minor(matrix, matrix(1,I)) * cofactor
 
-        cofactor = 1^(I+1); % Cofactor variable
+        cofactor = (-1)^(I+1); % Cofactor variable
         element = matrix(1,I); % Element variable
-        minor = find_minor(matrix, element); % use find minor function to find minor
+        minor = find_minor(matrix, 1, I); % use find minor function to find minor of top row elements
+        % disp([minor, cofactor, char_det]);
+       
         Current(I) = (voltage * minor * cofactor) / (char_det); % use equation for I
     end
+
 end
-I_values1 = Branch_Current_Calculation(V1, matrix0);
-disp(I_values1);
-% 
-% I_values = Branch_Current_Calculation(V1, matrix1);
-% disp (I_values);
+
+% assign calculated Current values to array
+I_values1 = Branch_Current_Calculation(V1, matrix1);
+
+for I = 1:3 % loop 3 times to print each current vallues
+    fprintf('Mesh %d Current Value: %f \n', I, I_values1(I)); %print current value
+end
+
